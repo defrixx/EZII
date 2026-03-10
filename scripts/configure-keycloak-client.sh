c724b21cfdef46e8f8c37260dc69e266174a4ec8
defrixx <defrixx@gmail.com>
2026-03-11 00:53:50 +0300

    v.0.1.5 - [Tested] - demo, registration, admin panel new features / bug fix-18

--- scripts/configure-keycloak-client.sh
+++ scripts/configure-keycloak-client.sh
@@ -96,7 +96,7 @@ mapper_exists() {
 
 is_uuid() {
   local value="$1"
-  [[ "${value}" =~ ^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$ ]]
+  [[ "${value}" =~ ^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$ ]]
 }
 
 backfill_missing_tenant_attributes() {
